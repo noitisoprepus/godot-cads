02389d965e8663fcfb8b6bfa96e72d544baba184
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2022-07-06 13:00:31 +0800

    Fused DialoguePlayer and DialogueParser into one

--- demo/Scripts/DialoguePlayer.gd
+++ demo/Scripts/DialoguePlayer.gd
@@ -3,44 +3,77 @@ extends Node
 signal dialogue_started
 signal dialogue_finished
 
-export(String, FILE, "*.json") var dialogue_json
+export(String) var d_key
+export(String) var d_name
+export(String, FILE, "*.json") var d_json
 
-const DialogueParser = preload("res://Scripts//DialogueParser.gd")
-var parser = DialogueParser.new()
+# TODO: Preload DialogueUI
 
-var dialogue_keys = []
-var dialogue_name: String = ""
-var dialogue_content: String = ""
-var current_state: String = ""  # Save this value somewhere to allow resuming
+var dialogue : Dictionary = {}
+var current_state: String = ""  # TODO: Save this value somewhere to allow resuming
 
-func start_dialogue():
+
+func start():
     emit_signal("dialogue_started")
     
     # Initial dialogue preparations
-    var dialogue = parser.load_dialogue(dialogue_json)
-    dialogue_keys.clear()
-    for key in dialogue:
-        dialogue_keys.append(dialogue[key])
-    
-    display_dialogue()
+    load_dialogue(d_json)
+    # TODO: Restore current_state value based on key
+    if (current_state == ""):
+        current_state = "Start"
+    next()
 
 
-func next_dialogue():
+func next():
     if current_state == "END":
-        # Close Dialogue UI
-        # Reset state
+        # TODO: Close Dialogue UI
+        # TODO: Reset state
         current_state = "Repeat"
         emit_signal("dialogue_finished")
         return
-    display_dialogue()
+    
+    # Process current_state
+    var current_event = process_state()
+    process_event(current_event)
 
 
-func display_dialogue():
-    # dialogue_content = dialogue_keys[current_dialogue_index].text
-    # dialogue_name = dialogue_keys[current_dialogue_index].name
+# TODO: Add an else statement for catching and displaying errors
+func load_dialogue(file):
+    var dialogue_file = File.new()
+    if dialogue_file.file_exists(file):
+        dialogue_file.open(file, dialogue_file.READ)
+        var dialogue_dict = parse_json(dialogue_file.get_as_text())
+        if typeof(dialogue_dict.result) == TYPE_DICTIONARY:
+            dialogue = dialogue_dict
+
+
+# Parse dialogue on the given state
+func process_state():
+    if dialogue.has(current_state):
+        var event: Dictionary = dialogue[current_state]
+        current_state = event["next"]
+        return event
+    
+    # display_message("Error. State not found")
+
+
+func process_event(event):
+    if event.has("dialogue"):
+        display_message(event["dialogue"])
     pass
+
+
+func display_message(content: Array):
+    print(content)
+    pass
+
+
+func display_choice(choices: Array):
+    print(choices)
+    pass
+
 
 # Think of a way to dynamically connect this to the player's interaction
 # func on_interact_pressed():
-#   start_dialogue()
+#   start()
 #   pass
