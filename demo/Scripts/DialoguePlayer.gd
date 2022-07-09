76cf4d2621a133d579f7b7d584a3e2724c0db770
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2022-07-09 23:48:59 +0800

    Did a little clean up

--- demo/Scripts/DialoguePlayer.gd
+++ demo/Scripts/DialoguePlayer.gd
@@ -1,36 +1,69 @@
 extends Node
 
-func queue_message(content: Array):
+signal dialogue_started
+signal dialogue_finished
+
+onready var dialogue_ui = get_node("/root/Game/UI/DialogueUI")
+
+var dialogue: Dictionary = {}
+var current_event: Dictionary = {}
+var current_state: String = ""
+
+# func start():
+	# Initial dialogue preparations
+	# _load_dialogue()
+	# TODO: Restore current_state value based on key
+	# if (current_state == ""):
+	# 	current_state = "Start"
+	# next()
+
+
+# func next():
+	# if current_state == "END":
+		# TODO: Close Dialogue UI
+		# TODO: Reset state
+	# 	current_state = "Repeat"
+	# 	return
+
+	# print("Processing dialogue")
+	# _process_state()
+
+
+func play(key, json_file):
+    _load_dialogue(json_file)
+
+    # Retrieve current_state value using key
+    _process_state()
+
+# TODO: Add an else statement for catching and displaying errors
+func _load_dialogue(path):
+    var dialogue_file = File.new()
+    if dialogue_file.file_exists(path):
+        dialogue_file.open(path, dialogue_file.READ)
+        dialogue = parse_json(dialogue_file.get_as_text())
+	# else:
+		# display_message(["Error. Dialogue JSON does not exist"])
+
+
+func _process_state():
+    if dialogue.has(current_state):
+        current_event = dialogue[current_state]
+        current_state = current_event["next"]
+        _process_event()
+	# else:
+	#   display_message(["Error. State" + current_state + " not found"])
+
+
+func _process_event():
     pass
 
 
-func display_message(message: String):
-    pass
+# Think of a way to dynamically connect this to the player's interaction
+# func on_interact_pressed():
+#   start()
+#   pass
 
 
-func continue_message():
-    pass
-
-
-func queue_choice(choices: Array):
-    pass
-
-
-func display_choice(choice: Dictionary):
-    pass
-
-
-func select_choice(choice):
-    pass
-
-
-func queue_function(command: Dictionary):
-    pass
-
-
-func exectute_function(command: Dictionary):
-    pass
-
-
-func start_queue():
-    pass
+# func on_next_pressed():
+#   next()
+#   pass
--- demo/Scripts/Interactable.gd
+++ demo/Scripts/Interactable.gd
@@ -1,92 +1,20 @@
 extends Node
 
-signal dialogue_started
-signal dialogue_finished
-
-export(String) var intertable_key
+export(String) var interactable_key
 export(String) var interactable_name
 export(String, FILE, "*.json") var dialogue_json
 
-onready var dialogue_player = get_node("/root/Node/DialogueUI")
+onready var dialogue_player = get_node("/root/Game/DialoguePlayer")
 
-var dialogue : Dictionary = {}
-var current_event: Dictionary = {}  # TODO: Save these values
-var current_state: String = ""      # somewhere to allow resuming
 var is_interacting: bool = false
-
-func start():
-	emit_signal("dialogue_started")
-	
-	# Initial dialogue preparations
-	_load_dialogue()
-	# TODO: Restore current_state value based on key
-	if (current_state == ""):
-		current_state = "Start"
-	print("Initializing dialogue")
-	next()
-
-
-func next():
-	if current_state == "END":
-		print("Ending dialogue")
-		# TODO: Close Dialogue UI
-		# TODO: Reset state
-		current_state = "Repeat"
-		emit_signal("dialogue_finished")
-		is_interacting = false
-		return
-
-	print("Processing dialogue")
-	_process_state()
-
-
-# TODO: Add an else statement for catching and displaying errors
-func _load_dialogue():
-	var dialogue_file = File.new()
-	if dialogue_file.file_exists(dialogue_json):
-		dialogue_file.open(dialogue_json, dialogue_file.READ)
-		dialogue = parse_json(dialogue_file.get_as_text())
-	# else:
-		# display_message(["Error. Dialogue JSON does not exist"])
-
-
-func _process_state():
-	if dialogue.has(current_state):
-		current_event = dialogue[current_state]
-		current_state = current_event["next"]
-		_process_event()
-	# else:
-	#   display_message(["Error. State" + current_state + " not found"])
-
-
-# TODO: Queue the events
-func _process_event():
-	if current_event.has("dialogue"):
-		for i in current_event["dialogue"].size():
-			print("Dialogue: " + current_event["dialogue"][i])
-		# dialogue_player.queue_message(current_event["dialogue"])
-	if current_event.has("choice"):
-		# dialogue_player.queue_choice(current_event["choices"])
-		pass
-	if current_event.has("function"):
-		# dialogue_player.queue_function(current_event["function"])
-		pass
-	# dialogue_player.start_queue()
-
-
-# Think of a way to dynamically connect this to the player's interaction
-# func on_interact_pressed():
-#   start()
-#   pass
-
-
-# func on_next_pressed():
-#   next()
-#   pass
 
 func _on_Button_pressed():
 	if not is_interacting:
 		is_interacting = true
-		start()
-	else:
-		next()
+		dialogue_player.play(interactable_key, dialogue_json)
+		dialogue_player.connect("dialogue_finished", self, "on_DialoguePlayerFinished")
+
+
+func _on_DialoguePlayer_finished():
+	is_interacting = false
+	dialogue_player.disconnect("dialogue_finished", self, "on_DialoguePlayerFinished")
